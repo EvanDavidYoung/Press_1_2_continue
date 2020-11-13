6f38a95bada2912876f2337a06c3d21f64497e4e
Evan Young <evanyoung@evans-mbp-4.lan>
2020-11-12 19:25:39 -0600

    got audio to work

--- main.py
+++ main.py
@@ -12,6 +12,33 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # import speech_recognition as sr 
+    import speech_recognition as sr
+    r = sr.Recognizer()
+    sample_audio = sr.AudioFile('TestFile/test.wav')
+    with sample_audio as audio_file:
+        audio_content = r.record(audio_file)
+    print(type(audio_content))
+    print(r.recognize_google(audio_content))
+
+    # with sr.Microphone() as source:                # use the default microphone as the audio source
+    #     audio = r.listen(source)                   # listen for the first phrase and extract it into audio data
+
+    # try:
+    #     print("You said " + r.recognize(audio))    # recognize speech using Google Speech Recognition
+    # except LookupError:                            # speech is unintelligible
+    #     print("Could not understand audio")
+
+    # r = sr.Recognizer()
+    # mic = sr.Microphone()
+    
+    # with mic as audio_file:
+    #     print("Speak Please")
+
+    #     r.adjust_for_ambient_noise(audio_file)
+    #     audio = r.listen(audio_file)
+
+    #     print("Converting Speech to Text...")
+    #     print("You said: " + r.recognize_google(audio))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ readme.txt
@@ -0,0 +1,4 @@
+If on mac: brew install portaudio
+Activate Envelope:
+source venv/Scripts/activate
+
